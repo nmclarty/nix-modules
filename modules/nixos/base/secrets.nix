7b0527ac47b291e138a8ee5ff9cba6cd1af2492e
Nevan McLarty <37232202+nmclarty@users.noreply.github.com>
2026-02-03 01:32:03 -0800

    Remove key conversion for sops-nix

--- modules/nixos/base/secrets.nix
+++ modules/nixos/base/secrets.nix
@@ -2,11 +2,7 @@
   sops = {
     defaultSopsFile = config.custom.base.secrets.global;
     log = [ "secretChanges" ];
-    age = {
-      generateKey = true;
-      sshKeyPaths = [ "/root/.ssh/id_ed25519" ];
-      keyFile = "/var/lib/sops-nix/key.txt";
-    };
+    age.keyFile = "/var/lib/sops-nix/key.txt";
     secrets = {
       # safe since it only contains public keys
       "known_hosts" = { mode = "0444"; };
